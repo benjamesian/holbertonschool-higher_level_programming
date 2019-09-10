0157440caf4eac898e0e39c7e5bd0ecb4be84c88
Ben Cook <808@holbertonschool.com>
2019-09-10 15:33:05 -0700

    remove variable usage

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-ord_a = ord('a')
-for el in range(ord_a, ord_a + 26):
-    print(chr(el), end='')
+for el in range(ord('a'), ord('a') + 26):
+    print("{}".format(chr(el)), end='')
