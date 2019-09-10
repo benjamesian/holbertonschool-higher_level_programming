193879d1c6a3017681a1417e41c13640a84cbdc7
Ben Cook <808@holbertonschool.com>
2019-09-10 16:52:21 -0700

    make pep8 compliant

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 1:
-        i = i - ord('a') +  ord('A')
+        i = i - ord('a') + ord('A')
     print("{}".format(chr(i)), end='')
