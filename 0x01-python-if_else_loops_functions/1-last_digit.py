67142fdcaaa668652ba49d24a0373950a09289bb
Ben Cook <808@holbertonschool.com>
2019-09-10 15:26:42 -0700

    fix typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 ld = (abs(number) % 10) * (-1 if number < 0 else 1)
 if ld == 0:
-    tail = "zero"
+    tail = "0"
 elif ld > 5:
     tail = "greater than 5"
 else:
