181d25b608deb2428ea94408004d725cb0f55d5a
Ben Cook <808@holbertonschool.com>
2019-09-10 15:41:13 -0700

    remove print call

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(99):
+for i in range(100):
     if (i < 10):
         print('0', end='')
-    print("{}".format(i), end=', ')
-print(99)
+    print("{}".format(i), end=(', ' if i < 99 else '\n'))
