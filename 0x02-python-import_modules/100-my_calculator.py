597f0355182d6a0416de42ba916377906e12848b
Ben Cook <808@holbertonschool.com>
2019-09-12 15:12:50 -0700

    revert to cleaner solution

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,26 +4,26 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) != 4:
+    def do_calc(a, op, b):
+        ops = [
+            ("+", add),
+            ("-", sub),
+            ("*", mul),
+            ("/", div)
+        ]
+
+        for oper in ops:
+            if op == oper[0]:
+                return oper[1](int(a), int(b))
+        return None
+
+    nargs = len(sys.argv)
+    if nargs != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    op = sys.argv[2]
-
-    if op not in "+-*/":
+    if sys.argv[2] not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    a1 = int(sys.argv[1])
-    a2 = int(sys.argv[3])
-
-    if op == "+":
-        result = add(a1, a2)
-    elif op == '-':
-        result = sub(a1, a2)
-    elif op == '*':
-        result = mul(a1, a2)
-    else:
-        result = div(a1, a2)
-
-    print("{:d} {} {:d} = {:d}".format(a1, op, a2, result))
+    print("{} {} {} = {}".format(*sys.argv[1:], do_calc(*sys.argv[1:])))
