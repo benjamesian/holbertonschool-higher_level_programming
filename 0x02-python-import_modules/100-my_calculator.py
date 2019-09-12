8a0116c5673be65730ccee024f6c460ed8d24366
Ben Cook <808@holbertonschool.com>
2019-09-12 14:50:44 -0700

    remove function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,31 +2,27 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+if __name__ == "__main__":
+    nargs = len(sys.argv)
+    if nargs != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-def do_calc(a, op, b):
+    a1 = sys.argv[1]
+    op = sys.argv[2]
+    a2 = sys.argv[3]
+
+    if sys.argv[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
     ops = [
         ("+", add),
         ("-", sub),
         ("*", mul),
         ("/", div)
     ]
-
     for oper in ops:
         if op == oper[0]:
-            return oper[1](int(a), int(b))
-    return None
-
-if __name__ == "__main__":
-    nargs = len(sys.argv)
-    a1 = sys.argv[1]
-    op = sys.argv[2]
-    a2 = sys.argv[3]
-    if nargs != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    if sys.argv[2] not in "+-*/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    print("{} {} {} = {}".format(a1, op, a2, do_calc(a1, op, a2)))
+            result = oper[1](int(a1), int(a2))
+    print("{} {} {} = {}".format(a1, op, a2, result))
