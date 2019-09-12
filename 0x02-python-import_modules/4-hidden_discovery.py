6655cca450ca147f46cfa6105912f58aa799798d
Ben Cook <808@holbertonschool.com>
2019-09-12 14:29:55 -0700

    use correct python version

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
-#!/home/benjames/Python-3.4.10/python
+#!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    so = dir('hidden_4.pyc')
+    so = dir(hidden_4)
     s = sorted(filter(lambda x: x[:2] != "__", so))
     for el in s:
         print(el)
