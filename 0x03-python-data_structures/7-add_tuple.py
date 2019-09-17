ad7ed9ff68b91298f6b5250a7bb92f7b91715609
Ben Cook <808@holbertonschool.com>
2019-09-17 15:26:31 -0700

    remove imported module

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from itertools import zip_longest
 def add_tuple(tuple_a=(), tuple_b=()):
-    l = list(zip_longest(tuple_a, tuple_b, (0,0), fillvalue=0))
+    l = list(zip(tuple_a + (0, 0), tuple_b + (0, 0)))
     return (sum(l[0]), sum(l[1]))
