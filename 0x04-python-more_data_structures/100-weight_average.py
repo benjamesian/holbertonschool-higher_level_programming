5298010a6ab80ac7925010ab4c9c5a8f65cd24c7
Ben Cook <808@holbertonschool.com>
2019-09-19 16:15:17 -0700

    attempt first advanced task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    prod = map(lambda x: x[0] * x[1], my_list)
+    return sum(prod) / sum(map(lambda x: x[1], my_list))
