51b1e9efe70a69ffab10fc495c2fa0613a0955c1
Ben Cook <808@holbertonschool.com>
2019-09-19 15:35:26 -0700

    simplify code with set type

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    my_list = sorted(my_list)
-    for i, el in enumerate(my_list):
-        if i > 0 and el == my_list[i - 1]:
-            continue
-        print(el)
-        sum += el
-
-    return sum
+    return sum(set(my_list))
