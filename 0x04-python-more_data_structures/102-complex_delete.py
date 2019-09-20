e1d56b70684ec633b75617e10701a533401524c8
Ben Cook <808@holbertonschool.com>
2019-09-19 20:30:01 -0700

    attempt complex delete task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    (a_dictionary.pop(k) for k in filter(lambda x: a_dictionary[x] == value, a_dictionary))
+    for k in list(filter(lambda x: a_dictionary[x] == value, a_dictionary)):
+        a_dictionary.pop(k)
     return a_dictionary
