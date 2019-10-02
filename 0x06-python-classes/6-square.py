e16f137b523fd6fe8d16bd43f7fec0fb27aa5aca
Ben Cook <808@holbertonschool.com>
2019-10-01 23:12:10 -0700

    change spacing output character

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,5 +48,5 @@ class Square:
             for _ in range(self.position[1]):
                 print('')
             for _ in range(self.size):
-                print("_" * self.position[0], end='')
+                print(" " * self.position[0], end='')
                 print("#" * self.size)
