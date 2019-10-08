bfa2682959766960cc832a5ef996a47685cd1502
Ben Cook <808@holbertonschool.com>
2019-10-07 22:05:11 -0700

    add dummy file for n-queens problem

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" N Queens """
+import sys
+
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    if argc != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    #I dont know anything about this problem :P
