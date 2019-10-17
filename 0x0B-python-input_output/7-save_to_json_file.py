9ab51be48ab183cfb3a31f4dc43073843b75e5fc
Ben Cook <808@holbertonschool.com>
2019-10-16 19:01:58 -0700

    attempt task 7

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" mod doc """
+
+
+def save_to_json_file(my_obj, filename):
+    """ func doc """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
