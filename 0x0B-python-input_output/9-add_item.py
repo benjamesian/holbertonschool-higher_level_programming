b2531f126ef15d6c55de74f64f25c42fc7b100ca
Ben Cook <808@holbertonschool.com>
2019-10-16 20:12:18 -0700

    use os module t check if file exists

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,15 +2,14 @@
 """ mod doc """
 import json
 import sys
-
+import os.path
 
 if __name__ == '__main__':
     fs = __import__('7-save_to_json_file').save_to_json_file
     fl = __import__('8-load_from_json_file').load_from_json_file
 
     filename = 'add_item.json'
-    with open(filename, 'w') as f:
-        if not f.read():
-            json.dump([], f)
-    obj = fl(filename)
-    fs(obj.extend(sys.argv), filename)
+    l = []
+    if os.path.exists(filename):
+        l = fl(filename) or []
+    fs(l + sys.argv.split(' '), filename)
