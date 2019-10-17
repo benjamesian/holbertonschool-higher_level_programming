309f2a52333782e5efca057729a17bed62c04826
Ben Cook <808@holbertonschool.com>
2019-10-17 16:25:08 -0400

    change print formatting

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,21 +3,23 @@
 import sys
 
 
-d = {}
-size = 0
-while True:
-    try:
-        for i in range(10):
-            line = sys.stdin.readline().split()
-            if line:
-                d[line[-2]] = d.get(line[-2], 0) + 1
-                size += int(line[-1])
-        print(f'File size: {size}')
-        for key in sorted(d):
-            print(f'{key}: {d[key]}')
-    except KeyboardInterrupt as inst:
-        print(f'File size: {size}')
-        for key in sorted(d):
-            print(f'{key}: {d[key]}')
+def my_print(d, size):
+    """ formatted printing """
+    print('File size: {:d}'.format(size))
+    for key in sorted(d):
+        print('{:s}: {:s}'.format(key, d[key]))
 
-        raise inst
+if __name__ == '__main__':
+    d = {}
+    size = 0
+    while True:
+        try:
+            for i in range(10):
+                line = sys.stdin.readline().split()
+                if line:
+                    d[line[-2]] = d.get(line[-2], 0) + 1
+                    size += int(line[-1])
+            my_print(d, size)
+        except KeyboardInterrupt as inst:
+            my_print(d, size)
+            raise inst
