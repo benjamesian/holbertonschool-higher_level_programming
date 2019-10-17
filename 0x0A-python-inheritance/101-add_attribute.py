2c97188c7d637190d027ab9aa1e5f1ed0e9aed8c
Ben Cook <808@holbertonschool.com>
2019-10-17 10:47:26 -0700

    attempt solution using hasattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" mod doc """
+
+
 def add_attribute(obj, name, value):
-    setattr(obj, name, value)
+    """ func doc """
+    if hasattr(obj, name):
+        setattr(obj, name, value)
