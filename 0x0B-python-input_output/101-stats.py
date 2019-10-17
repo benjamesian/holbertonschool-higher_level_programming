6f9c16c4342d3cd8770c090c2fe5412302421fb3
Ben Cook <808@holbertonschool.com>
2019-10-17 16:21:55 -0400

    remove if __name__...

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,23 +3,21 @@
 import sys
 
 
-if __name__ == '__main__':
-    d = {}
-    size = 0
-    while True:
-        try:
-            for i in range(10):
-                line = sys.stdin.readline().split()
-                if line:
-                    d[line[-2]] = d.get(line[-2], 0) + 1
-                    size += int(line[-1])
-            print(f'File size: {size}')
-            for key in sorted(d):
-                print(f'{key}: {d[key]}')
+d = {}
+size = 0
+while True:
+    try:
+        for i in range(10):
+            line = sys.stdin.readline().split()
+            if line:
+                d[line[-2]] = d.get(line[-2], 0) + 1
+                size += int(line[-1])
+        print(f'File size: {size}')
+        for key in sorted(d):
+            print(f'{key}: {d[key]}')
+    except KeyboardInterrupt as inst:
+        print(f'File size: {size}')
+        for key in sorted(d):
+            print(f'{key}: {d[key]}')
 
-        except KeyboardInterrupt as inst:
-            print(f'File size: {size}')
-            for key in sorted(d):
-                print(f'{key}: {d[key]}')
-
-            raise inst
+        raise inst
