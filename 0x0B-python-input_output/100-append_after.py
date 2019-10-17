719c281243cfb0daa7f2068c89262ae67003590e
Ben Cook <808@holbertonschool.com>
2019-10-17 16:27:26 -0700

    move variable into higher scope for clarity

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,8 +4,8 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ func doc """
+    text = ''
     with open(filename, 'r', encoding='utf-8') as f:
-        text = ''
         for line in f:
             text += line
             if search_string in line:
