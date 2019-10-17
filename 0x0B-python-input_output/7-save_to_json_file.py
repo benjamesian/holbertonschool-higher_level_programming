de67444534a5e149b488dc3c809836e457a7a95a
Ben Cook <808@holbertonschool.com>
2019-10-16 19:03:23 -0700

    remove encoding

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -4,5 +4,5 @@
 
 def save_to_json_file(my_obj, filename):
     """ func doc """
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
