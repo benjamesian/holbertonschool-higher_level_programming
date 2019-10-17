a1dc08caa0596088326271f000eca827642d64f6
Ben Cook <808@holbertonschool.com>
2019-10-17 16:18:57 -0400

    remove printing function

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
-"""mod doc
-
-this is my glorious docstring
-"""
+"""mod doc"""
 import sys
-
-
-def my_print(d, size):
-    """formatted printing
-
-    Parameters:
-    d (dict): a dictionary
-    size (int): a number
-    """
-    print(f'File size: {size}')
-    for key in sorted(d):
-        print(f'{key}: {d[key]}')
 
 
 if __name__ == '__main__':
@@ -28,7 +13,13 @@ if __name__ == '__main__':
                 if line:
                     d[line[-2]] = d.get(line[-2], 0) + 1
                     size += int(line[-1])
-            my_print(d, size)
+            print(f'File size: {size}')
+            for key in sorted(d):
+                print(f'{key}: {d[key]}')
+
         except KeyboardInterrupt as inst:
-            my_print(d, size)
+            print(f'File size: {size}')
+            for key in sorted(d):
+                print(f'{key}: {d[key]}')
+
             raise inst
