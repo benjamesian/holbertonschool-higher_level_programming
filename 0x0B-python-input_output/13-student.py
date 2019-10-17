44c1f6ed944c574473a35381bf00bc9b49aaed98
Ben Cook <808@holbertonschool.com>
2019-10-16 23:12:51 -0700

    attempt task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" mod doc """
+
+
+class Student:
+    """ Representation of a Student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ JSONified Student """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    d[attr] = self.__dict__[attr]
+            return d
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.key = json[key]
