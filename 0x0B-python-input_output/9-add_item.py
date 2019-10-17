202c395817e443e5b2eaea9f121cef25f52e9f9b
Ben Cook <808@holbertonschool.com>
2019-10-16 19:53:35 -0700

    attempt task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" mod doc """
+import json
+import sys
+
+
+if __name__ == '__main__':
+    fs = __import__('7-save_to_json_file').save_to_json_file
+    fl = __import__('8-load_from_json_file').load_from_json_file
+
+    filename = 'add_item.json'
+    with open(filename, 'w'):
+        pass
+    obj = fl(filename)
+    fs(obj.extend(sys.argv), filename)
