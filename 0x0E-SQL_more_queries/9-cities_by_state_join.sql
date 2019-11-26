9fc58d3661bfc42167ed2bfedce6890141a702ea
Ben Cook <808@holbertonschool.com>
2019-11-26 15:23:08 -0800

    include all cities using left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- List all cities with states
 SELECT C.id, C.name AS name, S.name as name
-FROM states S
-INNER JOIN cities C
+FROM cities C
+LEFT JOIN states S
 ON S.id = C.state_id
 ORDER BY C.id ASC;
