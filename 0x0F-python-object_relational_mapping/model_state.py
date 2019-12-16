b1e0647ef61e20a2ebeff6a2b2d90c820e04f32d
Ben Cook <808@holbertonschool.com>
2019-12-15 17:22:51 -0800

    make pep8 compliant

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, engine
 
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
