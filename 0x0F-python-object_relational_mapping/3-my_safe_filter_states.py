0b4174ff3b3f8546e6f201ce452d76e28852e195
Ben Cook <808@holbertonschool.com>
2019-12-15 16:31:02 -0800

    attempt task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    all_states = cur.execute("""
+    SELECT id, name
+    FROM states
+    WHERE name=%s
+    ORDER BY id ASC
+    """, (argv[4],))
+    for record in cur.fetchall():
+        print(record)
