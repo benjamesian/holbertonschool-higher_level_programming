f18d3db6dc9bed4e846dfb33994b226d3d01e593
Ben Cook <808@holbertonschool.com>
2019-12-15 22:07:06 -0800

    fix pep8 style

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     engine = create_engine(
         'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
     session = sessionmaker(bind=engine)()
-    q = session.query(City, State).filter(City.state_id == State.id).order_by(City.id)
+    q = session.query(City, State).filter(City.state_id == State.id)\
+        .order_by(City.id)
     for c, s in q.all():
         print('{}: ({}) {}'.format(s.name, c.id, c.name))
     # session.commit()
