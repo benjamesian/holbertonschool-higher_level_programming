12e44062dc9caf107a797ca89a249e350815d1ba
Ben Cook <808@holbertonschool.com>
2019-12-15 21:21:21 -0800

    attempt task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    session = sessionmaker(bind=engine)()
+    state = session.query(State)\
+        .filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    session.commit()
