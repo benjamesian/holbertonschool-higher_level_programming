409e5eec31e29a1e20516e073323acdaac1717ad
Ben Cook <808@holbertonschool.com>
2019-12-15 21:13:06 -0800

    attempt task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    session = sessionmaker(bind=engine)()
+    state_id = session.query(State.id)\
+        .filter(State.name == argv[4]).first()
+    print('{}'.format(state_id[0]))
