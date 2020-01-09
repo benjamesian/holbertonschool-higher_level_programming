443ee246b2c3e11952e08da9c37dcd536a0ad510
Ben Cook <808@holbertonschool.com>
2020-01-08 19:08:21 -0800

    attempt task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def find_peak_helper(start, stop, l):
+    if start >= stop:
+        return None
+
+    length = len(l)
+    mid = start + ((stop - start) // 2)
+    if mid > length:
+        return None
+    vl = l[mid - 1]
+    v = l[mid]
+    vr = l[(mid + 1) % length]
+
+    if vl == v == vr or vl < v > vr:
+        return v
+
+    lpeak = None if mid == stop else find_peak_helper(start, mid, l)
+    if lpeak is not None:
+        return lpeak
+    return None if mid == start else find_peak_helper(mid, stop, l)
+
+def find_peak(list_of_integers):
+    length = len(list_of_integers)
+    if length == 0:
+        return None
+    elif length <= 2:
+        return max(list_of_integers)
+    return find_peak_helper(0, length - 1, list_of_integers)
