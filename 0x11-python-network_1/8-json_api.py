1bf408a052b01583b30959fd3c61fb43eddc4076
Ben Cook <808@holbertonschool.com>
2020-01-14 18:07:01 -0800

    attempt task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    payload = {'q': '' if len(argv) <= 1 else argv[1]}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    try:
+        j = r.json
+        if not j:
+            print('No result')
+        else:
+            print('[{:s}] {:s}'.format(j.get('id'), j.get('name')))
+    except ValueError as e:
+        print('Not a valid JSON')
