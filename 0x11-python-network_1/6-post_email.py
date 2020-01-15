c7d10373dd8c2a9eec133de2b7e4c09aab307dca
Ben Cook <808@holbertonschool.com>
2020-01-14 17:57:17 -0800

    fix typo

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,6 +3,6 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    payload = {'email': arg[2]}
+    payload = {'email': argv[2]}
     r = requests.post(argv[1], data=payload)
     print(r.text)
