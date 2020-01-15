165b72743ca30a3ec4745c88d997792bd5fe89b6
Ben Cook <808@holbertonschool.com>
2020-01-14 17:44:15 -0800

    output type of response content

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {:s}'.format(r.encoding))
+    print('\t- type: {:s}'.format(str(type(r.content))))
     print('\t- content: {:s}'.format(r.content.decode('utf-8')))
