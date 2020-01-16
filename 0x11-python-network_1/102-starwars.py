69873a6e8f65a7328e1b48440e3bedff81b2c78f
Ben Cook <808@holbertonschool.com>
2020-01-15 21:42:19 -0800

    handle pagination

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -2,14 +2,21 @@
 import requests
 from sys import argv
 
+def print_page(json):
+    for result in json.get('results'):
+        print(result.get('name'))
+        for el in result.get('films'):
+            r = requests.get(el)
+            print('\t{}'.format(r.json().get('title')))
+
 if __name__ == '__main__':
     url = 'https://swapi.co/api/people/'
     payload = {'search': '' if len(argv) < 2 else argv[1]}
     r = requests.get(url, params=payload)
     json = r.json()
     print('Number of results: {}'.format(json.get('count')))
-    for result in json.get('results'):
-        print(result.get('name'))
-        for el in result.get('films'):
-            r = requests.get(el)
-            print('\t{}'.format(r.json().get('title')))
+    print_page(json)
+    while json.get('next'):
+        r = requests.get(json.get('next'))
+        json = r.json()
+        print_page(json)
