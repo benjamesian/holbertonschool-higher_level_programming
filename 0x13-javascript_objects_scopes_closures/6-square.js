de460d6d84218da62dede941a7379a6455a10e2c
Ben Cook <808@holbertonschool.com>
2020-02-10 14:13:16 -0800

    attempt task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      return;
-    }
-    this.width = w;
-    this.height = h;
-  }
+const Sq = require('./5-square');
 
-  print () {
-    const row = 'X'.repeat(this.width);
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Sq {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
@@ -41,4 +13,5 @@ class Square extends Rectangle {
     }
   }
 }
+
 module.exports = Square;
